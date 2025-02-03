a9cb767b72c8b414271218ff41bf1d6d023de0ba
firstencounter3 <needenergy27@gmail.com>
2025-02-03 12:27:32 +0700

    add cors middleware

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.router import router
-from db.db import create_db_and_tables, SessionDep
+from db.db import create_db_and_tables
 
 
 @asynccontextmanager
@@ -11,6 +12,18 @@ async def lifespan(app: FastAPI):
     create_db_and_tables()
     yield
 
+origins = [
+    "http://localhost:5173",
+    "http://localhost",
+]
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
